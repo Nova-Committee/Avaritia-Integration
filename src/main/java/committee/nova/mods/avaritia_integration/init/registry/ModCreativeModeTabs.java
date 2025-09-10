3e787c2f833bb1139767c86f5d1383e321b7934c
cu6 <3077476144@qq.com>
2025-09-10 23:48:33 +0800

    1.0.0

---
+++ src/main/java/committee/nova/mods/avaritia_integration/init/registry/ModCreativeModeTabs.java
@@ -0,0 +1,29 @@
+package committee.nova.mods.avaritia_integration.init.registry;
+
+import committee.nova.mods.avaritia.init.registry.ModItems;
+import committee.nova.mods.avaritia_integration.AvaritiaIntegration;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, AvaritiaIntegration.MOD_ID);
+    public static final List<RegistryObject<Item>> ACCEPT_ITEM = new ArrayList<>();
+
+    public static final RegistryObject<CreativeModeTab> CREATIVE_TAB = TABS.register("avaritia_integration_group", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.tab.Integration"))
+            .icon(()-> ModItems.infinity_catalyst.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                for (var item : ACCEPT_ITEM) {
+                    output.accept(item.get());
+                }
+
+            })
+            .build());
+}
---
+++ src/main/java/committee/nova/mods/avaritia_integration/init/registry/ModItems.java
@@ -0,0 +1,11 @@
+package committee.nova.mods.avaritia_integration.init.registry;
+
+import committee.nova.mods.avaritia_integration.AvaritiaIntegration;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AvaritiaIntegration.MOD_ID);
+
+}
