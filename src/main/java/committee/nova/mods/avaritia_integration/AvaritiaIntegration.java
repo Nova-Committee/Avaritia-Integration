7a47dde91cbdc2a9f402d9c98f0f5d6e1af00f1b
cu6 <3077476144@qq.com>
2025-09-10 23:02:29 +0800

    1.0.0

---
+++ src/main/java/committee/nova/mods/avaritia_integration/AvaritiaIntegration.java
@@ -0,0 +1,58 @@
+package committee.nova.mods.avaritia_integration;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(AvaritiaIntegration.MOD_ID)
+public class AvaritiaIntegration
+{
+
+    public static final String MOD_ID = "avaritia_integration";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AvaritiaIntegration()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+
+    }
+
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            
+        }
+    }
+}
