1b988a0226e9b743434ff307a33b91024c147419
Y-Xiao233 <1870377449@qq.com>
2026-05-23 00:17:15 +0800

    修复了部分配方缺失的问题

--- integrations/industrialforegoing/src/main/java/committee/nova/mods/avaritia_integration/integrations/industrialforegoing/datagen/IndustrialForegoingRecipes.java
+++ integrations/industrialforegoing/src/main/java/committee/nova/mods/avaritia_integration/integrations/industrialforegoing/datagen/IndustrialForegoingRecipes.java
@@ -1,13 +1,22 @@
 package committee.nova.mods.avaritia_integration.integrations.industrialforegoing.datagen;
 
+import com.buuz135.industrial.recipe.LaserDrillFluidRecipe;
+import com.buuz135.industrial.recipe.LaserDrillRarity;
+import com.buuz135.industrial.recipe.data.EntityData;
 import committee.nova.mods.avaritia_integration.integrations.industrialforegoing.item.AddonItem;
+import committee.nova.mods.avaritia_integration.integrations.industrialforegoing.registry.IndustrialForegoingIntegrationFluids;
 import committee.nova.mods.avaritia_integration.integrations.industrialforegoing.registry.IndustrialForegoingIntegrationItems;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.world.entity.EntityType;
+import net.neoforged.neoforge.fluids.crafting.SizedFluidIngredient;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
@@ -18,9 +27,14 @@ public class IndustrialForegoingRecipes extends RecipeProvider {
     }
 
     @Override
-    protected void buildRecipes(RecipeOutput consumer) {
+    protected void buildRecipes(@NotNull RecipeOutput consumer) {
         for (Supplier<? extends AddonItem> supplier : IndustrialForegoingIntegrationItems.ADDONS.values()) {
             supplier.get().registerRecipe(consumer);
         }
+
+        var elderlyMedullaRecipe = new LaserDrillFluidRecipe(SizedFluidIngredient.of(IndustrialForegoingIntegrationFluids.ELDERLY_MEDULLA.getSourceFluid().get(),50),7, Optional.of(EntityData.of(EntityType.ELDER_GUARDIAN)), new LaserDrillRarity(new LaserDrillRarity.BiomeRarity(new ArrayList<>(), new ArrayList<>()), new LaserDrillRarity.DimensionRarity(new ArrayList<>(),new ArrayList<>()),-64, 256, 8));
+        var voidMatterRecipe = new LaserDrillFluidRecipe(SizedFluidIngredient.of(IndustrialForegoingIntegrationFluids.VOID_MATTER.getSourceFluid().get(),20),15, Optional.empty(), new LaserDrillRarity(new LaserDrillRarity.BiomeRarity(LaserDrillRarity.BiomeRarity.END, new ArrayList<>()), new LaserDrillRarity.DimensionRarity(new ArrayList<>(),new ArrayList<>()),-32, 64, 8));
+        LaserDrillFluidRecipe.createRecipe(consumer,"elderly_medulla","industrialforegoing",elderlyMedullaRecipe);
+        LaserDrillFluidRecipe.createRecipe(consumer,"void_matter","industrialforegoing",voidMatterRecipe);
     }
 }
